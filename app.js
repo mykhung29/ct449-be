7353d13596c0ca8cea203c7530adfdb16a1a80a0
Huu <tranhuumy2902@gmail.com>
2023-09-22 15:40:12 +0700

    Cai dat xu ly loi

--- app.js
+++ app.js
@@ -2,10 +2,21 @@ const express = require("express");
 const cors = require("cors");
 const contactsRouter = require("./app/routes/contact.route");
 const app = express();
+const ApiError = require("./app/api-error");
 
 app.use(cors());
 app.use(express.json());
 app.use("/api/contacts", contactsRouter);
+
+app.use((req, res, next) => {
+  return next(new ApiError(404, "Resource not found"));
+});
+
+app.use((err, req, res, next) => {
+  return res.status(error.statusCode || 500).json({
+    message: error.message || "Internal Server Error",
+  });
+});
 
 app.get("/", (req, res) => {
   res.json({ mesage: "welcome to contact book application" });
