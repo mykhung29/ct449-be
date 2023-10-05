3239a529899180c7967c80b78cc63d29afd75311
Huu <tranhuumy2902@gmail.com>
2023-10-05 17:08:38 +0700

    Cai dat cac handler truy xuat CSDL

--- app/services/contact.service.js
+++ app/services/contact.service.js
@@ -19,16 +19,66 @@ class ContactService {
     return contact; 
 }
 
-async create (payload) {
-    const contact = this.extractContactData(payload)
-    const result = await this.Contact.findOneAndUpdate(
-        contact,
-        { $set: { favorite: contact.favorite === true } },
-        { returnDocument: 'after', upsert: true }
-    )
+    async create (payload) {
+        const contact = this.extractContactData(payload)
+        const result = await this.Contact.findOneAndUpdate(
+            contact,
+            { $set: { favorite: contact.favorite === true } },
+            { returnDocument: 'after', upsert: true }
+        )
+        
+        return result;  }
     
-    return result; 
-}
+    async find (filter) {
+        const cursor = await this.Contact.find(filter)
+        return await cursor.toArray()
+    };  
+
+    async findByName (name) {
+        return await this.find ({
+            name: { $regex: new RegExp(name), $options: 'i'},
+        })
+    };
+    async findByID (id) {
+        return await this.Contact.findOne({
+            _id: ObjectId.isValid(id) ? new ObjectId(id) : null,
+        })
+    }
+
+    async update (id, payload) {
+        let filter = {
+            _id: ObjectId.isValid(id) ? new ObjectId(id) : null, 
+        }
+
+        const newContact = this.extractContactData(payload)
+        const result = await this.Contact.findOneAndUpdate(
+            filter,
+            { $set: newContact },
+            { returnDocument: 'after' }
+        )
+    }
+
+    async delete (id) {
+        const result = this.Contact.findOneAndDelete({
+            _id: ObjectId.isValid(id) ? new ObjectId(id) : null,
+        })
+
+        return result
+    }
+
+    async findAllFavorite () {
+        return await this.find({
+            favorite: true
+        })
+    }
+
+    async deleteAll () {
+        const result = await this.Contact.deleteMany({ })
+
+        return result.deletedCount
+    }
+
+    
 
 // Định nghĩa các phương thức truy xuất CSDL sử dụng mongodb API
 };
